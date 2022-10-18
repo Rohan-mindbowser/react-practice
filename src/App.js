9500a2add1e6292cd16c2bde0a800895838bacf6
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-10-18 12:26:14 +0530

    Implemented automatic redirect to specific url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 import { Profile } from "./components/Profile/Profile";
 import { Home } from "./components/Home/Home";
 import { createContext } from "react";
@@ -15,8 +21,9 @@ function App() {
     <>
       <AppContext.Provider value={{ name, setName }}>
         <Router>
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/book/:id">Books</NavLink>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const Notfound = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  }, []);
+
   return (
     <>
       <h1>Not found page</h1>
